f5e06f6855283fdc845a3fb44932d4175e46f745
Jaime Jiménez Ríos <jaimejimenezrios10@gmail.com>
2020-12-16 10:31:43 +0100

    añadido hola mundo

---
+++ proyecto_java/src/com/entronosdesarrollo/App.java
@@ -0,0 +1,15 @@
+package com.entronosdesarrollo;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hola Mundo");
+		
+		
+		
+	}
+
+}
