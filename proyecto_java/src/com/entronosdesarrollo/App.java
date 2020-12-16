248fef98fcfefdf05e05cb5260b9ba8f50171b31
Jaime Jiménez Ríos <jaimejimenezrios10@gmail.com>
2020-12-16 10:44:29 +0100

    Saludo a nombre que se pide por teclado

--- proyecto_java/src/com/entronosdesarrollo/App.java
+++ proyecto_java/src/com/entronosdesarrollo/App.java
@@ -7,8 +7,9 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println("Hola Mundo");
+		System.out.println("Introduce tu nombre:");
+		String nombre = leerStringPorConsola();
+		System.out.println("Hola, "+nombre);
 		
 		
 		
